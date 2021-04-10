1f0921970e027e0a27a72a3a41c3bc0fc7ac3776
David <david.chui@outlook.com>
2021-04-10 11:55:28 -0600

    Stock Overview tab

--- Denali/Startup.cs
+++ Denali/Startup.cs
@@ -28,7 +28,8 @@ namespace Denali
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<WeatherForecastService>();     // Inversion of Control
+            services.AddHttpClient(); //adds HttpClientFactory into dependency injection system.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
